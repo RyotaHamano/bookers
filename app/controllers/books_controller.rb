422f2e01ffab62adf68403a9da76afc72f5e80e3
EC2 Default User <ec2-user@ip-172-31-12-31.ap-northeast-1.compute.internal>
2023-07-06 05:38:12 +0000

    [Add]課題2-各ビューファイル作成

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,41 @@
+class BooksController < ApplicationController
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to "/books/#{book.id}"
+    
+  end
+
+  def index
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def show
+   @book = Book.find(params[:id])
+  end
+
+  def edit
+   @book = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    @book.update
+    redirect_to book_path(@book)
+  end 
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end 
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end 
+  
+end
